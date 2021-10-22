bc0815d6bbd20cf2cc91549717b79d82c9d0ded4
Taty <tatyanaparks@gmail.com>
2021-10-21 19:50:27 -0500

    added css transitions on food hover

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -17,14 +17,20 @@ export const Food = styled(Title)`
     height: 100px;
     padding: 10px;
     font-size: 20px;
-    background-image: ${({img}) => `url(${img});`}
+    background-image: ${({ img }) => `url(${img});`}
     background-position: center;
     background-size: cover;
-    filter: contrast(90%);
+    filter: contrast(75%);
     border-radius: 7px;
-    box-shadow: 0px 0px 10px 0px grey;
+    margin-top: 5px;
+    transition-property: box-shadow margin-top filter;
+    transition-duration: .1s;
+    box-shadow: 0px 0px 2px 0px grey;
     &:hover {
         cursor: pointer;
-        opacity: 0.7;
+        filter: contrast(100%);
+        margin-top: 0px;
+        margin-bottom: 5px;
+        box-shadow: 0px 0px 10px 0px grey;
     }
-`
+`;
