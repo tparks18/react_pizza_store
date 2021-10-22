e9cc0840c96b4410e066853ecec21bc2733d81ff
Taty <tatyanaparks@gmail.com>
2021-10-21 19:44:48 -0500

    add sections

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,22 +1,36 @@
-export const foods = [
+export const foodItems = [
   {
     name: "Pepperoni Pizza",
     img: "/img/pepperoni-pizza.jpg",
+    section: "Pizza"
   },
   {
     name: "Spinach Pizza",
     img: "/img/spinach-pizza.jpg",
+    section: "Pizza"
   },
   {
     name: "Fries",
     img: "/img/fries.jpg",
+    section: "Sides"
   },
   {
     name: "Hamburger",
     img: "/img/hamburger.jpg",
+    section: "Sandwiches"
   },
   {
     name: "Chicken Tenders",
     img: "/img/chicken-tenders.jpg",
-  }
+    section: "Sides"
+  },
 ];
+
+
+export const foods = foodItems.reduce((res, food) => {
+  if (!res[food.section]) {
+    res[food.section] = [];
+  }
+  res[food.section].push(food);
+  return res;
+}, {});
